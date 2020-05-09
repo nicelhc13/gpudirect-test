b3cd3302bd98eb4609487f1ea8e410b7c66ab8c5
Hochan Lee <hochan@utexas.edu>
2020-05-09 16:58:20 -0500

    Add parallel comm. mode and fix some bugs

--- src/cpu_bench.cpp
+++ src/cpu_bench.cpp
@@ -5,12 +5,13 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
+#include <omp.h>
 #include "mpi.h"
 
 #define NODES      2
 
 #ifdef ENABLE_MULTI_MSG
-#define MAX_COMM   100
+#define MAX_COMM   3000
 #else
 #define MAX_COMM   1
 #endif
@@ -22,13 +23,13 @@
 #endif
 
 void initializeBuffers(int *send_buffer) {
-  for (int i = 0; i < MAX_COMM; i++) {
+  for (int i = 0; i < MSG_SIZE; i++) {
     send_buffer[i] = i;
   }
 }
 
 void printBuffer(int *buffer) {
-  for (int i = 0; i < MAX_COMM; i++) {
+  for (int i = 0; i < MSG_SIZE; i++) {
     printf("\tbuffer[%d] = %d ", i, buffer[i]);
   }
   printf("\n");
@@ -36,7 +37,7 @@ void printBuffer(int *buffer) {
 
 void verifyRecvedBuffers(int *send_buffer,
                          int *reduce) {
-  for (int i = 0; i < MAX_COMM; i++) {
+  for (int i = 0; i < MSG_SIZE; i++) {
     if (send_buffer[i] != i) {
       printf("%d is failed to verified; %d\n", i, send_buffer[i]);
       *reduce += 1;
@@ -51,7 +52,7 @@ void verifyRecvedBuffers(int *send_buffer,
 
 int main(int argc, char** argv) {
   int rank, p;
-  int *buffer[MAX_COMM];
+  int *buffer;
   int supportProvided;
 
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &supportProvided);
@@ -62,27 +63,30 @@ int main(int argc, char** argv) {
          rank, p);
   printf("Number of nodes: %d, Number of msgs: %d,"
          "Msg size: %d\n", NODES, MAX_COMM, MSG_SIZE);
+#ifdef PARALLEL_MSG_MODE
+  omp_set_num_threads(56);
+  printf("Parallel mode is enabled. Used number of threads is 56\n");
+#endif
 
   //! Initialize buffers.
-  for (int i = 0; i < MAX_COMM; i++) {
-    buffer[i] = (int *) malloc(MSG_SIZE*sizeof(int));
-  }
+  buffer = (int *) malloc(MSG_SIZE*sizeof(int));
 
   if (rank == 0) { ///< Rank0 node.
-    for (int i = 0; i < MAX_COMM; i++) {
-      initializeBuffers(buffer[i]);
-    }
     printf("RANK 0: Initialize msg..\n");
+    initializeBuffers(buffer);
     printf("RANK 0: Send msg..\n");
+#ifdef PRINT_BUFFER
+    printf("RANK 0: Printing send-msg %d-th\n", i);
+    printBuffer(buffer);
+#endif
     //! Send the msg one by one.
     for (int neigh = 1; neigh < NODES; neigh++) {
+#ifdef PARALLEL_MSG_MODE
+      #pragma omp parallel for
+#endif
       for (int i = 0; i < MAX_COMM; i++) {
         printf("RANK 0: Sending msg %d-th\n", i);
-#ifdef PRINT_BUFFER
-        printf("RANK 0: Printing send-msg %d-th\n", i);
-        printBuffer(buffer[i]);
-#endif
-        MPI_Send(buffer[i], MSG_SIZE, MPI_INT,
+        MPI_Send(buffer, MSG_SIZE, MPI_INT,
                  neigh, 0, MPI_COMM_WORLD);
         printf("RANK 0: Sending msg %d-th to %d: done\n", i, neigh);
       }
@@ -90,31 +94,30 @@ int main(int argc, char** argv) {
     printf("RANK 0: Rank 0 is done\n");
   } else { ///< Not rank0 nodes.
     //! Initialize receiver-side buffers.
-    for (int i = 0; i < MAX_COMM; i++) {
-      memset(buffer[i], 0, sizeof(int)*MSG_SIZE);
+    memset(buffer, 0, sizeof(int)*MSG_SIZE);
 #ifdef PRINT_BUFFER
-      printf("RANK %d: Print recv buffer before recving\n", rank, i);
-      printBuffer(buffer[i]);
+    printf("RANK %d: Print recv buffer before recving\n", rank);
+    printBuffer(buffer);
 #endif
-    }
     int reduce;
     //! Receive the msg one by one.
     for (int i = 0; i < MAX_COMM; i++) {
       reduce = 0;
       printf("RANK %d: Tries to recv %d-th msg (size: %d)\n", rank, i, MSG_SIZE);
-      MPI_Recv(buffer[i], MSG_SIZE, MPI_INT, 0,
+      MPI_Recv(buffer, MSG_SIZE, MPI_INT, 0,
                0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 #ifdef PRINT_BUFFER
       printf("RANK %d: Print recv %d-th msg\n", rank, i);
-      printBuffer(buffer[i]);
+      printBuffer(buffer);
 #endif
       printf("Starts to verifying.. %d-th msg\n", i);
-      verifyRecvedBuffers(buffer[i], &reduce); 
+      verifyRecvedBuffers(buffer, &reduce); 
       printf("Verified done.. %d-th msg\n", i);
     }
     printf("RANK %d: Received msg\n", rank);
   }
 
   MPI_Finalize();
+  free(buffer);
   return 0;
 }
